3307d47870ace1df283222a5082ae27e84e8d72d
Rohan Dongre <dongrerohan421@gmail.com>
2023-09-24 14:57:21 -0700

    reuse Employee class

--- src/main/java/org/example/biconsumer/Java8BiConsumer.java
+++ src/main/java/org/example/biconsumer/Java8BiConsumer.java
@@ -1,19 +1,11 @@
 package org.example.biconsumer;
 
 
+import org.example.predicate.Employee;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
-
-class Employee {
-    String name;
-    double salary;
-
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-}
 
 public class Java8BiConsumer {
     public static void main(String[] args) {
@@ -21,14 +13,15 @@ public class Java8BiConsumer {
         System.out.println("Salary increment example: ");
         List<Employee> employees = new ArrayList<>();
         populate(employees);
-        BiConsumer<Employee, Double> salIncrement =  (employee, increment) -> employee.salary += increment;
+        BiConsumer<Employee, Integer> biConsumer = Employee::seteSalary;
 
-        for (Employee e: employees) {
-            salIncrement.accept(e, 500.0);
+        for (Employee e : employees) {
+            int increment = e.geteSalary() + 500;
+            biConsumer.accept(e, increment);
         }
 
-        for (Employee e: employees){
-            System.out.println("EName: " + e.name+ " salary increment: " + e.salary);
+        for (Employee e : employees) {
+            System.out.println("EName: " + e.geteName() + " salary increment: " + e.geteSalary());
         }
     }
 
